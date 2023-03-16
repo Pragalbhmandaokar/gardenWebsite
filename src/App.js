bb86ec565a999d354cdd1971cd6caf2f9a9e9a43
Pragalbh mandaokar <pragalbh.a.mandaokar@gmail.com>
2023-03-16 18:02:47 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import background from "./images/bg.jpg";
 function App() {
   return (
     <div className="w-full bg-[#042511fb]">
-      <div className="w-full px-10 py-4">
+      <div className="w-full px-2 md:px-10 py-4">
         <div
-          className="w-full h-[95vh]  flex flex-col justify-center items-center   shadow-black shadow-xl rounded-3xl border-[1px] border-green-900"
+          className="w-full h-[90vh] md:h-[95vh]  flex flex-col justify-center items-center   shadow-black shadow-xl rounded-3xl border-[1px] border-green-900"
           style={{
             backgroundImage: `url(${background})`,
             backgroundSize: "cover",
@@ -16,7 +16,7 @@ function App() {
           }}
         >
           <div className="w-full h-[100%] bg-[#00000066] rounded-3xl ">
-            <div className="text-[#ffffffdc] w-full flex justify-center items-center pt-20 text-[40px] tracking-widest font-extrabold ">
+            <div className="text-[#ffffffdc] w-full flex justify-center items-center text-center pt-20 text-[40px] tracking-widest font-extrabold ">
               MYRITIE'S GARDEN
             </div>
             <div className="flex w-full flex-col px-24 items-center justify-center  mt-10 text-white">
