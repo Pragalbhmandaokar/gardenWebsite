1295c763eabce6c210c47949f371bf7f790d813f
Pragalbh mandaokar <pragalbh.a.mandaokar@gmail.com>
2023-03-16 17:58:31 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "./css/font.css";
+import logo from "./images/m1.png";
+import background from "./images/bg.jpg";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="w-full bg-[#042511fb]">
+      <div className="w-full px-10 py-4">
+        <div
+          className="w-full h-[95vh]  flex flex-col justify-center items-center   shadow-black shadow-xl rounded-3xl border-[1px] border-green-900"
+          style={{
+            backgroundImage: `url(${background})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div className="w-full h-[100%] bg-[#00000066] rounded-3xl ">
+            <div className="text-[#ffffffdc] w-full flex justify-center items-center pt-20 text-[40px] tracking-widest font-extrabold ">
+              MYRITIE'S GARDEN
+            </div>
+            <div className="flex w-full flex-col px-24 items-center justify-center  mt-10 text-white">
+              <div className="text-center fontApercu text-[40px] font-extrabold">
+                CURRENTLY <br></br>OUT OF SERVICE
+              </div>
+              <div className="mt-2 flex items-center justify-center">
+                <div className="w-10 h-[2px] bg-white rounded-full"></div>
+                <div className="mx-2 w-20 flex justify-center items-center">
+                  <img src={logo} className="" />
+                </div>
+                <div className="w-10 h-[2px] bg-white rounded-full"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
